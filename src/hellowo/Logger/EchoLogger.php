e83b327ef63ef28228c633240290a861fb8e7be3
ryunosuke <ryunosuke.arima@gmail.com>
2025-08-14 22:01:28 +0900

    [feature] EchoLogger にレベルフィルタを実装
    - テストで全部出るとうざいことがある

--- src/hellowo/Logger/EchoLogger.php
+++ src/hellowo/Logger/EchoLogger.php
@@ -4,14 +4,30 @@ namespace ryunosuke\hellowo\Logger;
 
 use DateTime;
 use Psr\Log\AbstractLogger;
+use Psr\Log\LogLevel;
+use ReflectionClass;
 
 /**
  * @codeCoverageIgnore this is reference implementation
  */
 class EchoLogger extends AbstractLogger
 {
+    private $level;
+
+    public function __construct($level)
+    {
+        $this->level = $level;
+    }
+
     public function log($level, $message, array $context = [])
     {
+        static $levels = null;
+        $levels ??= array_flip(array_values((new ReflectionClass(LogLevel::class))->getConstants()));
+
+        if ($levels[$this->level] < $levels[$level]) {
+            return;
+        }
+
         $now = (new DateTime())->format('Y-m-d\\TH:i:s.v');
 
         echo "[$now][$level] $message\n";
