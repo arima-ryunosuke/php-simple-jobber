f43bfabc908a04f8f4a17c61994c0c1145799cc5
ryunosuke <ryunosuke.arima@gmail.com>
2025-10-18 20:26:01 +0900

    [change] interpolate で階層のないリスト配列を特別扱いする
    - 連想配列は a.b.c の可能性があるので除外
    - 階層がなければ json でシンプルに埋め込める
    - いずれにせよ InterpolationTrait は開発向けであり、運用時はしっかりとした logger を使うはず

--- src/hellowo/Logger/InterpolationTrait.php
+++ src/hellowo/Logger/InterpolationTrait.php
@@ -6,7 +6,21 @@ trait InterpolationTrait
 {
     private function interpolate(string $message, array $context): string
     {
-        $main = function ($message, array $context, array $parents) use (&$main) {
+        $array_is_list = function (array $array): bool {
+            if (function_exists('array_is_list')) {
+                return array_is_list($array); // @codeCoverageIgnore
+            }
+
+            $i = 0;
+            foreach ($array as $k => $v) {
+                if ($k !== $i++) {
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        $main = function ($message, array $context, array $parents) use (&$main, $array_is_list) {
             foreach ($context as $key => $value) {
                 if (preg_match('#\A[a-zA-Z0-9_.]+\z#u', $key)) {
                     $keys   = $parents;
@@ -21,8 +35,13 @@ trait InterpolationTrait
                         }
                     }
                     if (is_array($value)) {
-                        $message = $main($message, $value, $keys);
-                        continue;
+                        if (count($value) === count($value, COUNT_RECURSIVE) && $array_is_list($value)) {
+                            $value = json_encode($value, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+                        }
+                        else {
+                            $message = $main($message, $value, $keys);
+                            continue;
+                        }
                     }
 
                     if (is_null($value) || is_bool($value)) {
--- tests/Test/hellowo/Logger/InterpolationTraitTest.php
+++ tests/Test/hellowo/Logger/InterpolationTraitTest.php
@@ -11,16 +11,17 @@ class InterpolationTraitTest extends AbstractTestCase
 
     function test_interpolate()
     {
-        that($this)->interpolate('{null}, {double}+{double}, {string}, {array}, {array.a}, {array.b}, {object}, {object.c}, {stringable}, {undefined}', [
+        that($this)->interpolate('{null}, {double}+{double}, {string}, {list}, {array}, {array.a}, {array.b}, {object}, {object.c}, {stringable}, {undefined}', [
             'null'       => null,
             'double'     => 3.14,
             'string'     => 'S',
+            'list'       => [1, 2, 3],
             'array'      => [
                 'a' => 'A',
             ],
             'array.b'    => 'B',
             'object'     => (object) ['c' => 'C'],
             'stringable' => new class { public function __toString(): string { return 'Stringgable'; } },
-        ])->is('NULL, 3.14+3.14, S, {array}, A, B, {object}, C, Stringgable, {undefined}');
+        ])->is('NULL, 3.14+3.14, S, [1,2,3], {array}, A, B, {object}, C, Stringgable, {undefined}');
     }
 }
